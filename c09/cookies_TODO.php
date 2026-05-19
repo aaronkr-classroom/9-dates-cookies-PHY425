98dea8496dba171cde92621e2e2eb7857fd25c26
에런스노버거 <jekkilekki@gmail.com>
2026-05-19 13:22:57 +0900

    Update ch8, 9 code files

---
+++ c09/cookies_TODO.php
@@ -0,0 +1,8 @@
+<?php
+// @TODO
+?>
+<?php include 'includes/header.php'; ?> 
+
+<h1><?= $greeting ?></h1>
+
+<?php include 'includes/footer.php'; ?> 
---
+++ c09/login_TODO.php
@@ -0,0 +1,11 @@
+<?php
+// @TODO
+?>
+<?php include 'includes/header-member.php'; ?>
+<h1>Login</h1>
+<form method="POST" action="login.php">
+  Email: <input type="email" name="email"><br>
+  Password: <input type="password" name="password"><br>
+  <input type="submit" value="Log In">
+</form>
+<?php include 'includes/footer.php'; ?>
